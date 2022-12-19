850bb5ca573c3b57bb28c64ef23fc2f3409ad704
Nikolai Lukin <niklu100@gmail.com>
2022-12-19 14:56:39 +0500

    first

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        String a = "red";
+
+        switch (a) {
+            case "red":
+                System.out.println("stop");
+                break;
+            case "yellow":
+                System.out.println("get ready");
+                break;
+            case "green":
+                System.out.println("go");
+                break;
+            default:
+                System.out.println("error");
+                break;
+        }
+    }
+}
